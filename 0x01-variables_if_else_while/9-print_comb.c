52cea869fdbfa1c8665ca95f7c2ec8cbd9bc0e25
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-20 14:10:48 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,23 +8,15 @@
 int main(void)
 {
     int n ;
-    int sep;
     for(n=48; n!=58;n++)
     {
         putchar(n);
         if(n!=57)
         {
-            sep =44;
-        }
-        else
-        {
-           sep = 36;
-        }
-        putchar(sep);
-        if (n!=57)
-        {
+            putchar(',');
             putchar(' ');
         }
+       
     }
     
     putchar('\n');
