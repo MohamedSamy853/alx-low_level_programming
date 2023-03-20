41b037a50aaeb5082fc35508bd6b0e2e2937b890
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-20 13:56:49 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main -this is a main function
+ * 
+ * Return:0 when closed
+ * */
+int main(void)
+{
+    int n ;
+    for(n=48; n!=58;n++)
+    {
+        putchar(n);
+        if(n!=57)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        else
+        {
+            putchar('$');
+        }
+    }
+    
+    putchar('\n');
+    return(0);
+}
