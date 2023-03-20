812d6fb5e80e6c9943ad9a2217d4de025266138f
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-20 13:30:21 -0700

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main -this is a main function
+ *
+ * Return:0 when closed
+ */
+int main(void)
+{
+	int i=0;
+	for(i=0; i!=10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return(0);
+}
