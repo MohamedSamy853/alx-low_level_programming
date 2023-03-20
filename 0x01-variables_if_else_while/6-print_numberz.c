46627874b03a1eba3d2a969c23df52e886550206
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-20 13:39:06 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main -this is a main function
+ * 
+ * Return:0 when closed
+ * */
+int main(void)
+{
+    int i =0;
+    for (i=48; i!=58;i++)
+    {
+        putchar(i);
+    }
+   
+    putchar('\n');
+    return(0);
+}
