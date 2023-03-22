0e5084ec3b373c598564448d59fb796001af7f26
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-22 12:52:45 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_sign - function to print sign of anumber
+ *
+ * Return:1 ,+ if n > zero
+ * and 0,0 if n is zero 
+ * and -1 , - if n<0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}	
