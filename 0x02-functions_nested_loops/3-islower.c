7d2f3149098b5c90883083c631d9101700ee1bfb
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-22 12:31:58 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower : check if lettrt is lower
+ *
+ * Return : 0 when closed
+ */
+int _islower(int c )
+{
+ 	if ( c >= 97 && c < 122 )
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
