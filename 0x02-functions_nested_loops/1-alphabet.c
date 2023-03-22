5a66731a12e98f83fd0775872f7b168dfb1a3bf2
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-22 12:12:52 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - program start here
+ *
+ * print_alphabetc - print alphapetc no return
+ *
+ * Return :0 when terminited
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
