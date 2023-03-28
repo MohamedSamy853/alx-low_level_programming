d3c5439fa4a218b29d06a02046f6745170514d17
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 07:49:43 -0700

    task-6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		if (count % 2 == 0)
 			_putchar(*str);
 		str++;
-		count++;	
+		count++;
 	}
 	_putchar('\n');
 }
