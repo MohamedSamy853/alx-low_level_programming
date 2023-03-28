ac94f5a41795f26604d2cab9fa1fd795f81d8a04
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 06:16:16 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - a function tr reveser string
+ * @s: is a pointer of array of char
+ * Return: is is a void number
+ */
+void print_rev(char *s)
+{
+	char rev;
+	int counter = 0;
+	int i;
+
+	while (*s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
