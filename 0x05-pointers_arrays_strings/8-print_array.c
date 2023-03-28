2730c346cda947dc171249e84c209bbf82e740eb
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 08:35:37 -0700

    task-8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - is a function that print array
+ * @a: is a pointer of array
+ * @n: is a number of elements which nedded for print
+ * Return: is a void function
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
