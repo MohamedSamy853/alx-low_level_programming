cc0915acb44696a2d6856c9d3e4c08f125aca23c
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 08:19:02 -0700

    task-7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,16 @@ void puts_half(char *str)
 	int i;
 	char *s = str;
 	int len = 0;
-
+	
 	while (*str != '\0')
 	{
 		str++;
 		len++;
 	}
 	start = len / 2;
+	if (start % 2 == 1)
+		start = (len - 1) / 2;
+
 	for (i = start ; i < len ; i++)
 	{
 		_putchar(s[i]);
