95a4ef07041081fb1075626ef3a1fe7aa45b7b9e
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 05:50:18 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - is a function that take array of char and print it
+ * @str: if a pointer
+ * Return: it is a void function
+ */
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
