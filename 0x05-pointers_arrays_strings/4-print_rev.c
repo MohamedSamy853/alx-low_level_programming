1feee2d317afeec0997f3c86842843ab0b5d2dfe
MohamedSamy853 <mohamed.samy.2248369@gmail.com>
2023-03-28 06:31:34 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - is a function to print reverse string
+ * @s: is a pointer for array of char
+ * Return: it is a void function
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (o = len - 1; o >= 0 ; o--)
+	{
+		_putchar(s[o]);
+	}
+	_putchar('\n');
+}
